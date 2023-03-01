d73c92bb86d7bb1946c7cb27e12e54461d407c54
diegofachin <diegofachin@gmail.com>
2023-03-01 16:06:15 -0300

    feat: ajustes

--- Infra/Repositories/GenericRepository.cs
+++ Infra/Repositories/GenericRepository.cs
@@ -1,11 +1,6 @@
 ﻿using Domain.Interfaces;
 using Infra.Context;
 using Microsoft.EntityFrameworkCore;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Infra.Repositories;
 
